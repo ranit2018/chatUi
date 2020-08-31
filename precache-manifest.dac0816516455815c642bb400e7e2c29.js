c1c71187616875a65dc4f7080535f2c8514bbb77
ranit2018 <iamranit81@gmail.com>
2020-08-31 18:46:08 +0530

    adding files

---
+++ precache-manifest.dac0816516455815c642bb400e7e2c29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cd4632996d5599137cf101b42f25cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76362bf62669ae822db",
+    "url": "/static/css/main.163e283b.chunk.css"
+  },
+  {
+    "revision": "46ad42e80103dee63975",
+    "url": "/static/js/2.3238ac6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3238ac6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76362bf62669ae822db",
+    "url": "/static/js/main.150e8360.chunk.js"
+  },
+  {
+    "revision": "f4a2a66cbda01c297e27",
+    "url": "/static/js/runtime-main.354e05e4.js"
+  }
+]);
